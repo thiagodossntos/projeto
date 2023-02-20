b6daef770a5f0330490f594bbe7d7400922855a5
thiago Santos <125689037+thiagodossntos@users.noreply.github.com>
2023-02-20 10:44:06 -0300

    Add files via upload
    projeto 2

---
+++ anos3000.py.py
@@ -0,0 +1,4 @@
+print("Em que ano você nasceu?")
+nascimento = input()
+nascimento = int(nascimento)
+print("Em 3000 você terá ", 3000 - nascimento, " anos!" )
---
+++ anosdecachorro.py.py
@@ -0,0 +1,9 @@
+print("Você quer saber sua idade em anos caninos?")
+print("Se sim, quantos anos você tem?")
+nascimento = input()
+nascimento = int(nascimento)
+print("Se você fosse um cachorrom, você teria" , nascimento * 7 , "!" )
+print("""
+"Auu" '0'________'  
+         | |  | |
+   """)
